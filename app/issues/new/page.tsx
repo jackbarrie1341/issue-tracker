d141e658c31c9414ea73e47d5312ce653a0bc79b
jackbarrie1341 <jackbarrie1341@gmail.com>
2023-10-24 22:20:44 -0600

    implement client side validation

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,20 +1,27 @@
 "use client";
-import { Button, Callout, TextField } from "@radix-ui/themes";
+import { Button, Callout, Text, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import { useForm, Controller } from "react-hook-form";
 import "easymde/dist/easymde.min.css";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from "@/app/validationSchemas";
+import { z } from "zod";
 
-interface IssueForm {
-	title: string;
-	description: string;
-}
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
 	const router = useRouter();
-	const { register, control, handleSubmit } = useForm<IssueForm>();
+	const {
+		register,
+		control,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<IssueForm>({
+		resolver: zodResolver(createIssueSchema),
+	});
 	const [error, setError] = useState("");
 
 	return (
@@ -41,6 +48,11 @@ const NewIssuePage = () => {
 						{...register("title")}
 					/>
 				</TextField.Root>
+				{errors.title && (
+					<Text color="red" as="p">
+						{errors.title.message}
+					</Text>
+				)}
 
 				{/* SimpleMDE doesnt support registering props like TextField.Input, so need to use this */}
 				<Controller
@@ -50,6 +62,11 @@ const NewIssuePage = () => {
 						<SimpleMDE placeholder="Description" {...field} />
 					)}
 				/>
+				{errors.description && (
+					<Text color="red" as="p">
+						{errors.description.message}
+					</Text>
+				)}
 
 				<Button>Submit New Issue</Button>
 			</form>
